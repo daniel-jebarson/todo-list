ea066e4d64020b99009e0d946b9ad9ef732b7b7f
daniel-jebarson <danieljebarson21@gmail.com>
2022-11-02 15:16:09 +0530

    Add : Provider 🎭

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../hooks/index";
+import { ThemeProvider } from "next-themes";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
+  );
 }
