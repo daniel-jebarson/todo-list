8858b0814624239534d8a95860882edb54662577
daniel-jebarson <danieljebarson21@gmail.com>
2022-11-03 13:30:56 +0530

    update: todo component 🪙

--- components/todo.tsx
+++ components/todo.tsx
@@ -1,4 +1,56 @@
+import { AddIcon, DeleteIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  ButtonGroup,
+  Container,
+  IconButton,
+  Text,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators, State } from "../hooks";
+
 const Todo = (props: { display: number }) => {
-  return <h1>No of Tasks:{props.display}</h1>;
+  const dispatch = useDispatch();
+  const [desc, setDesc] = useState("");
+  const { ADDTODO, REMOVETODO } = bindActionCreators(actionCreators, dispatch);
+  const state = useSelector((state: State) => state.todo);
+  return (
+    <Container
+      justifyContent={"center"}
+      display="flex"
+      flexDirection={"column"}
+      mt="80px"
+    >
+      <Text ml={"40%"}>No of Tasks:{state.noOfTasks}</Text>
+
+      <Container
+        justifyContent={"center"}
+        display="flex"
+        flexDirection={"column"}
+        mt="40px"
+        gap={"40px"}
+      >
+        {state.data.map((val, index) => {
+          return (
+            <ButtonGroup size="sm" isAttached variant="outline" gap={"30px"}>
+              <Text>{val.description}</Text>
+              <IconButton
+                onClick={() => {
+                  REMOVETODO();
+                }}
+                w={8}
+                h={8}
+                color="red.500"
+                aria-label="Remove"
+                icon={<DeleteIcon />}
+              />
+            </ButtonGroup>
+          );
+        })}
+      </Container>
+    </Container>
+  );
 };
 export default Todo;
